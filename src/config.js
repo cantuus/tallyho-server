c55976ca678a2262ef0511422162ae787fe00483
Sophia Koeut <koeutsophia@gmail.com>
2020-01-07 22:05:56 -0800

    changed DB_URL to DATABASE_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/tallyho',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/tallyho',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/tallyho-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
--- test/tasks-endpoints.spec.js
+++ test/tasks-endpoints.spec.js
@@ -14,7 +14,7 @@ describe('Tasks Endpoints', function () {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.TEST_DATABASE_URL,
         })
         app.set('db', db)
     })
